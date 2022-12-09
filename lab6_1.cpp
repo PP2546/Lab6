04cb200a7145b5ab24aeb76788817b2545d8b732
PP <phiphatphong_ka@cmu.ac.th>
2022-12-09 15:49:42 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int N ,i=0 , j=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    
+    while(N!=0){
+        if(N%2 == 0){
+                    i=i+1;
+                        }else{
+                    j=j+1;
+                }
+        cout << "Enter an integer: ";
+        cin >> N;
+        }
+        
+    cout << "#Even numbers = " << i << "\n";
+    cout << "#Odd numbers = " << j << "\n";
     return 0;
 }
